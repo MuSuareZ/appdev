b43857754cd4299bf12e398f3a9fddf3ab0a40c5
MuSuareZ <murillo.finlandia@gmail.com>
2019-04-09 12:24:38 +0100

    barChart and colored informations

--- sound.c
+++ sound.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 #include "sound.h"
+#include "screen.h"
 
 // this function takes 1 second of samples (16000 in our case)
 // and calculate 80 pieces of RMS value and then
 // turn these valyes into decibels, and display them as
 // a barchart
+int pickCount = 0;
 void displayWAVDATA(short s[]) {
 	double rms[80];		// because we have 16000 samples, 16000 / 80 = 200
 				// so every 200 samples makes one RMS
-	int i, j;		// nested loop counters
+	int db[80];
+	int i, j, k = 0;	// nested loop counters
 	short *ptr = s;		// use pointers to point to the beginning of the samples
 
 	for(i = 0; i < 80; i++) {
@@ -19,8 +22,18 @@ void displayWAVDATA(short s[]) {
 			ptr++;				// pointer increments
 		}
 		rms[i] = sqrt(sum/200);
+		db[i] = 20*log10(rms[i]);		// decibel value
+
+		if(db[i] > 75) {
+			pickCount += 1;
+		}
+#ifdef DEBUG						// conditional compilation
 		printf("RMS[%d] = %f\n", i, rms[i]);
-	}
+#endif
+	}	//end of for loop
+#ifndef DEBUG
+	barChart(db);
+#endif
 }
 void showID(char *name, char *value) {
 	printf("%s: ", name);
@@ -31,6 +44,7 @@ void showID(char *name, char *value) {
 }
 // function definition
 void displayWAVHDR(struct WAVHDR h){
+#ifdef DEBUG
 	showID("Chunk ID", h.ChunkID);
 	printf("Chunk size: %d\n", h.ChunkSize);
 	showID("Format", h.Format);
@@ -44,4 +58,20 @@ void displayWAVHDR(struct WAVHDR h){
 	printf("Bits per sample: %d\n", h.BitsPerSample);
 	showID("Subchunk 2 ID: ", h.Subchunk2ID);
 	printf("Subchunk 2 size: %d\n", h.Subchunk2Size);
+#else
+	setColors(RED, bg(BLUE));
+	printf("\033[1;H");
+	printf("test.wav            \n");
+	setColors(YELLOW, bg(GREEN));
+	printf("\033[1;21H");
+	printf("Sample rate=%d      \n", h.SampleRate);
+	setColors(WHITE, bg(MAGENTA));
+	printf("\033[1;41H");
+	printf("Duration=%.2f sec   \n", (float)h.Subchunk2Size/h.ByteRate);
+	setColors(BLACK, bg(GREEN));
+	printf("\033[1;61H");
+	printf("Peaks=%d            \n", pickCount);
+	resetColors();
+	pickCount = 0;
+#endif
 }
